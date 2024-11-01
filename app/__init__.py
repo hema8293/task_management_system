bddef1918a54cf6f3b0e82e254f5fabb21768633
hema8293 <jd.saravanan93@gmail.com>
2024-11-01 13:22:29 -0400

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate = Migrate(app, db)
+
+    from .routes import main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
