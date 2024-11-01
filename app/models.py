15d88546f29e1a00876eee29d322bb2be0cc1d20
hema8293 <jd.saravanan93@gmail.com>
2024-11-01 13:23:09 -0400

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from . import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    tasks = db.relationship('Task', backref='owner', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
