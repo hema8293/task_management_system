46ba4f2aabacba97690f3a462ba7149732bbe774
hema8293 <jd.saravanan93@gmail.com>
2024-11-01 13:23:51 -0400

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,38 @@
+from flask import render_template, redirect, url_for, flash, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from . import db
+from .models import User, Task
+from .forms import RegistrationForm, LoginForm
+
+main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.dashboard'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('main.login'))
+    return render_template('register.html', form=form)
+
+@main_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.dashboard'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('main.dashboard'))
+    return render_template('login.html', form=form)
+
+@main_blueprint.route('/dashboard')
+@login_required
+def dashboard():
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return render_template('dashboard.html', tasks=tasks)
